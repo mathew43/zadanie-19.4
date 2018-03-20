fd4f6b2ba903f88a2599302c4b3a7194e5f41134
Mateusz Łukowski <mateusz.lukowski@wp.pl>
2018-03-20 15:27:11 +0100

    add edit and thumps cases

--- comments.js
+++ comments.js
@@ -16,6 +16,38 @@ function reducer (state = initialState, action){
         comments: state.comments.filter(comment => comment.id !== action.id)
       });
 
+    case actions.EDIT_COMMENT:
+      return [
+          state.map( comment => {
+            return comment.id === action.id ?
+              Object.assign({}, comment, {
+                  text: action.text
+                  }) :
+                comment
+          })
+      ]
+
+    case actions.THUMB_UP_COMMENT:
+        return [
+            state.map( comment => {
+                return comment.id === action.id ?
+                    Object.assign({}, comment, {
+                        votes: comment.votes + 1
+                    }) :
+                    comment
+            })
+        ]
+
+    case actions.THUMB_DOWN_COMMENT:
+        return [
+            state.map ( comment => {
+                return comment.id === action.id ?
+                    Object.assign({}, comment, {
+                        votes: comment.votes - 1
+                    }) :
+                    comment
+            })
+        ]
     default:
       return state;
   }
